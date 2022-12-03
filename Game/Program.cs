0a1ebf23bbd58552b4a7a97be19614a5751148b0
casxavier <s10223292@connect.np.edu.sg>
2022-12-03 15:02:46 +0800

    Display Main Menu

--- Game/Program.cs
+++ Game/Program.cs
@@ -6,7 +6,54 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                DisplayMenu();
+                Console.Write("Please choose an option: ");
+                try
+                {
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice > 2 || choice < 0)
+                    {
+                        Console.WriteLine("Please enter a valid choice\n");
+                        Console.Write("Please enter your option: ");
+                        choice = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    else if (choice == 1)
+                    {
+                    }
+                    else if (choice == 2)
+                    {
+                    }
+                    else if (choice == 3)
+                    {
+                    }
+                    else if (choice == 4)
+                    {
+                    }
+
+                }
+                catch (FormatException choiceError)
+                {
+                    Console.WriteLine(choiceError.Message + "\nPlease enter a valid choice\n");
+                }
+                catch (OverflowException choiceError)
+                {
+                    Console.WriteLine(choiceError.Message + "\nPlease enter a valid choice\n");
+                }
+            }
+        }
+        static void DisplayMenu()
+        {
+
+            Console.WriteLine("\n---------------MENU---------------");
+            Console.WriteLine("[1] Start New Game");
+            Console.WriteLine("[2] Load Saved Game");
+            Console.WriteLine("[3] Display High Scores");
+            Console.WriteLine("[0] Exit Game");
+            Console.WriteLine("----------------------------------\n");
+
         }
     }
 }
